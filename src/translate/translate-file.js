3747671bf0d57f6c4f4f70fba08b306f8693e3e8
Dmitry <merkulov.design@gmail.com>
2025-02-09 17:42:20 +0200

    Fix texted translation issue

--- src/translate/translate-file.js
+++ src/translate/translate-file.js
@@ -2,6 +2,41 @@ import {translateSentence} from "./translate-sentence";
 import {readJson} from "../disk/read-json";
 
 /**
+ * Recursively translate the JSON object
+ * @param obj
+ * @param args
+ * @param targetTranslation
+ * @param isOverride
+ * @return {Promise<void>}
+ */
+async function translateJsonObject(obj, args, targetTranslation, isOverride) {
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof value === 'object' && value !== null) {
+      if (!targetTranslation[key]) {
+        targetTranslation[key] = {};
+      }
+      await translateJsonObject(value, args, targetTranslation[key], isOverride);
+    } else {
+      if (!isOverride && targetTranslation[key] !== undefined) {
+        console.info(`Skipping ${key}`);
+        continue;
+      }
+
+      const translation = await translateSentence(value, args);
+      if (translation === null) {
+        console.warn(`Failed to translate ${key}`);
+        continue;
+      }
+
+      targetTranslation[key] = translation;
+
+      // Add a 1-second timeout
+      await new Promise(resolve => setTimeout(resolve, args['timeout'] ?? 1000));
+    }
+  }
+}
+
+/**
  * Translate the source file to the target
  * @param args
  * @return {Promise<{}>}
@@ -12,27 +47,7 @@ export async function translateFile(args) {
 
   const isOverride = (args['override'] ?? 'false') === 'true';
 
-  for (const [key, value] of Object.entries(sourceTranslations)) {
-
-    // If the key is already in the target translation and override is false, skip
-    if (!isOverride && targetTranslation[key] !== undefined) {
-      console.info(`Skipping ${key}`);
-      continue;
-    }
-
-    const translation = await translateSentence(value, args);
-    if (translation === null) {
-      console.warn(`Failed to translate ${key}`);
-      continue;
-    }
-
-    // Add the translation to the target translation
-    targetTranslation[key] = translation;
-
-    // Add a 1-second timeout
-    await new Promise(resolve => setTimeout(resolve, args['timeout'] ?? 1000));
-
-  }
+  await translateJsonObject(sourceTranslations, args, targetTranslation, isOverride);
 
   // If the target translation is empty, the translation failed
   if (Object.keys(targetTranslation).length !== Object.keys(sourceTranslations).length) {
