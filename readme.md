5d74289fb1c97208e914def2641aefbee2f7a699
Dmitry <merkulov.design@gmail.com>
2025-04-03 15:29:10 +0300

    Refactoring options

--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
 ```terminal
-perekladach-json translate source=./file.json from=en to=uk override=false timeout=500
+perekladach-json translate -- -source=./file.json -from=en -to=uk -override=false -delay=500
 ```
+
+#### Options
+
+| Option         | Values                 | Description                                           |
+|----------------|------------------------|-------------------------------------------------------|
+| --f --from     | Language code(ISO 639) | Source language (e.g., en, uk)                        |
+| --t --to       | Language code(ISO 639) | Target language (e.g., en, uk)                        |
+| --s --source   |                        | Source file path (e.g., ./file.json)                  |
+| --o --override | true of false          | Override existing translations (default: false)       |
+| --d --delay    | 0 to 999999            | Delay between requests in milliseconds (default: 500) |
+| --l --log      | info, verbose or none  | Log level (default: info)                             |
 
 ```dotenv
 PEREKLADACH_OPENAI_API_KEY=""
--- src/tools/define-args.js
+++ src/tools/define-args.js
@@ -4,29 +4,103 @@
  */
 export function defineArgs() {
 
-  const args = process.argv.slice(2); // Slice to remove node and script name
-
+  // Check if the script is being run directly
+  const args = process.argv.slice(2); // Remove 'node' and script path
   if (args.length === 0) {
     console.error("No arguments provided.");
     process.exit(1);
   }
 
-  let options = {};
-  args.forEach(argString => {
-
-    // Split by '='
-    const argsSpited = argString.split('=');
-
-    let key = argsSpited[0].trim();
-    let value = argsSpited[1].trim();
-    if (key === 'to') {
-      value = value.split(',').map(v => v.trim());
+  // Parse the arguments
+  const parsedArgs = args.reduce((acc, arg) => {
+    const [key, value] = arg.split('=');
+    if (key.startsWith('-')) {
+      acc[formatKey(key)] = formatValue(value);
     }
+    return acc;
+  }, {});
 
-    options[key] = value;
+  // Check if the required arguments are present
+  if (parsedArgs.length === 0) {
+    console.error("No arguments passed.");
+    process.exit(1);
+  }
 
-  });
+  console.log("Parsed arguments:", parsedArgs);
 
-  return options;
+  return parsedArgs;
+
+}
+
+/**
+ * Format the key by removing leading dashes
+ * @param key
+ */
+function formatKey( key ) {
+
+  key = key.replace(/^-+/, ''); // Remove leading dashes
+  const shortKeys = {
+    'f': 'from',
+    't': 'to',
+    's': 'source',
+    'o': 'override',
+    'd': 'delay',
+    'l': 'log',
+    'h': 'help',
+  };
+
+  // Check if the key is a short key
+  if (shortKeys[key]) {
+    return shortKeys[key];
+  }
+
+  return key; // Return the key as is
+
+}
+
+/**
+ * Format the value based on its content
+ * @param value
+ * @return {number|{}|*|null|boolean}
+ */
+function formatValue( value ) {
+
+  switch (value) {
+
+    case 'true':
+      return true;
+
+    case 'false':
+      return false;
+
+    case 'null':
+      return null;
+
+    default:
+
+      // If string contains , convert to array
+      if (value.includes(',')) {
+        return value.split(',').map(item => item.trim());
+      }
+
+      // If string contains : convert to object
+      if (value.includes(':')) {
+        const obj = {};
+        value.split(',').forEach(item => {
+          const [key, val] = item.split(':');
+          obj[key.trim()] = val.trim();
+        });
+        return obj;
+      }
+
+      // If string is only digits, convert to number
+      if (!isNaN(value)) {
+        return Number(value);
+      }
+
+      // Otherwise, return the string as is
+      return value;
+
+  }
 
 }
